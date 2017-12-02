675395ca1ffd970258e2470e0b3a144b662cecd8
kheeva <kheeva@ya.ru>
2017-12-02 09:48:22 +0300

    get_user and get_password were deleted. json_friends_online renamed to list_friends_online, print fixed.

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -6,14 +6,6 @@ import vk
 APP_ID = 6280715
 
 
-def get_user_login(login=input('Input login: ')):
-    return login
-
-
-def get_user_password(password=getpass.getpass('Input password: ')):
-    return password
-
-
 def get_online_friends(login, password):
     session = vk.AuthSession(
         app_id=APP_ID,
@@ -26,18 +18,20 @@ def get_online_friends(login, password):
 
 
 def output_friends_to_console(friends_online, api):
-    json_friends_online = api.users.get(user_ids=friends_online)
-    for friend in json_friends_online:
-        print('\nThere are friends online:')
+    list_friends_online = api.users.get(user_ids=friends_online)
+    print('\nThere are friends online:')
+    for friend in list_friends_online:
         print(' {} {}'.format(friend['first_name'], friend['last_name']))
 
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
+    login = input('Input login: ')
+    password = getpass.getpass('Input password: ')
     try:
         friends_online, api = get_online_friends(login, password)
     except vk.exceptions.VkAuthError:
         pass
+    except vk.exceptions.VkAPIError:
+        print('Don\'t forget to input your username and password.')
     else:
         output_friends_to_console(friends_online, api)
