9561c51c5f11db406a6daf5a607e2b623a6f7d62
kheeva <kheeva@ya.ru>
2017-12-02 09:58:34 +0300

    get_online_friends() and output_friends_to_console() were refactored

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -14,13 +14,12 @@ def get_online_friends(login, password):
         scope='friends'
     )
     api = vk.API(session)
-    return api.friends.getOnline(), api
+    list_friends_online_id = api.friends.getOnline()
+    return api.users.get(user_ids=list_friends_online_id)
 
 
-def output_friends_to_console(friends_online, api):
-    list_friends_online = api.users.get(user_ids=friends_online)
-    print('\nThere are friends online:')
-    for friend in list_friends_online:
+def output_friends_to_console(friends_online):
+    for friend in friends_online:
         print(' {} {}'.format(friend['first_name'], friend['last_name']))
 
 
@@ -28,10 +27,11 @@ if __name__ == '__main__':
     login = input('Input login: ')
     password = getpass.getpass('Input password: ')
     try:
-        friends_online, api = get_online_friends(login, password)
+        friends_online = get_online_friends(login, password)
     except vk.exceptions.VkAuthError:
         pass
     except vk.exceptions.VkAPIError:
         print('Don\'t forget to input your username and password.')
     else:
-        output_friends_to_console(friends_online, api)
+        print('\nThere are friends online:')
+        output_friends_to_console(friends_online)
